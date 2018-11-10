8f2df0b8ca642edd5161810ea080d1b2a472c6a6
Adreena Howard <adreena.howard@yahoo.com>
2018-11-10 13:01:37 -0600

    added to js

--- bamazon.sql
+++ bamazon.sql
@@ -5,7 +5,7 @@ USE bamazon_DB;
 
 
 CREATE TABLE products(
-	id INT NOT NULL,
+	item_id INT(10) NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(40),
     department_name VARCHAR(40),
     price DECIMAL(10,2),
@@ -13,18 +13,18 @@ CREATE TABLE products(
    
 );
 
-INSERT INTO products(id, product_name, department_name, price, stock_quantity)
+INSERT INTO products(item_id, product_name, department_name, price, stock_quantity)
 VALUES   
 
-(1, 'Rancilio Silvia', 'Espresso machine', 550, 5),
-(2, 'Rocket R58', 'Espresso machine', 3000, 5 ),
-(3, 'Rocket Porta', 'Espresso machine', 2650, 2),
-(4, 'Ascaso Dream', 'Espresso machine', 599, 8),
-(5, 'Baratza Sette', 'Grinder', 549, 3),
-(6, 'Nuova Simonelli', 'Grinder', 1012.50, 4),
-(7, 'Mazzer Jolly', 'Grinder', 1199, 1),
-(8, 'Breville Dose', 'Grinder', 179.95, 15),
-(9, 'Hario V60', 'Scale', 52.00, 7),
-(10,'Brewista Smart', 'Scale', 89.95, 5);
+( 'Rancilio Silvia', 'Espresso machine', 550, 5),
+( 'Rocket R58', 'Espresso machine', 3000, 5 ),
+( 'Rocket Porta', 'Espresso machine', 2650, 2),
+( 'Ascaso Dream', 'Espresso machine', 599, 8),
+( 'Baratza Sette', 'Grinder', 549, 3),
+( 'Nuova Simonelli', 'Grinder', 1012.50, 4),
+( 'Mazzer Jolly', 'Grinder', 1199, 1),
+( 'Breville Dose', 'Grinder', 179.95, 15),
+( 'Hario V60', 'Scale', 52.00, 7),
+('Brewista Smart', 'Scale', 89.95, 5);
 
 SELECT * FROM products;
